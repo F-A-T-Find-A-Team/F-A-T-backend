13c5b94f650fed3f133f385125d959488a33b6ad
louisrui0028 <louisrui0028@gmail.com>
2026-08-26 08:33:19 +0900

    Revert "Revert "1 feat login signup""

---
+++ src/main/java/com/F_A_T/F_A_T/FATApplication.java
@@ -0,0 +1,13 @@
+package com.F_A_T.F_A_T;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FATApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FATApplication.class, args);
+	}
+
+}
